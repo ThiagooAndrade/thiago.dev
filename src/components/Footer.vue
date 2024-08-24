bc3bd00e50dc0edb9c1e9e65a1951f44fddb0595
ThiagooAndrade <andradethiago1609@Gmail.com>
2024-08-24 20:51:39 -0300

    feat: themeMode injection

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { inject, ref } from 'vue';
+import { themeColor } from '../context/keys';
+import { themeColorType } from '../types/Types';
+
+const themeMode = ref(inject<themeColorType>(themeColor) as themeColorType);
+
+function themeModeClass() {
+    return {
+        titleColor: themeMode.value.isDarkMode ? "text-gray-200" : "text-[#42446E]"
+    }
+}
+
+</script>
 
 <template>
-  
+  <div>
+    
+  </div>
 </template>
