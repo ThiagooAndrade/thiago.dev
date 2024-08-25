7c68fe868320025919200155409e3a1f5f077481
ThiagooAndrade <andradethiago1609@Gmail.com>
2024-08-25 17:47:03 -0300

    feat: footer container

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,14 +7,18 @@ const themeMode = ref(inject<themeColorType>(themeColor) as themeColorType);
 
 function themeModeClass() {
     return {
-        titleColor: themeMode.value.isDarkMode ? "text-gray-200" : "text-[#42446E]"
+        paragraphColor: themeMode.value.isDarkMode ? "text-gray-200" : "text-black"
     }
 }
 
 </script>
 
 <template>
-  <div>
-    
-  </div>
+  <section class="mt-20">
+    <div class="container px-4 md:px-8 lg:px-16">
+      <span class="block h-[1px] bg-gray-500"></span>
+      <div class="py-4 text-end">
+        <p :class="themeModeClass().paragraphColor">Desenvolvido por Thiago 2024</p></div>
+    </div>
+  </section>
 </template>
