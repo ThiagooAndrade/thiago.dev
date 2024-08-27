622bd4ab2fd1337e364bc04b4609e338ca96381e
ThiagooAndrade <andradethiago1609@Gmail.com>
2024-08-26 23:06:16 -0300

    add: AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+
+
+</script>
 
 <template>
-  About
+  <main class="container p-4 md:px-8 lg:px-16">
+    <section>
+      <h1>About me</h1>
+      <div>
+        <p></p>
+      </div>
+    </section>
+  </main>
 </template>
