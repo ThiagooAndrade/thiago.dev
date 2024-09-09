b8867ce52a4cf010546382f463881449686f54f3
ThiagooAndrade <andradethiago1609@Gmail.com>
2024-09-09 16:11:16 -0300

    add: links

--- src/data/ProjectsData.ts
+++ src/data/ProjectsData.ts
@@ -1,3 +1,8 @@
+export const links = {
+  github: "https://github.com/ThiagooAndrade",
+  linkedin: "https://www.linkedin.com/in/thiagoo-andrade/",
+}
+
 export const projectsData = [
   {
     img: "meutempo.png",
@@ -6,5 +11,6 @@ export const projectsData = [
     livePreviewUrl: "https://meu-tempo.vercel.app/",
     githubUrl: "https://github.com/ThiagooAndrade/Meu-tempo",
     stack: ["HTML", "CSS", "JAVASCRIPT", "OpenWeatherMap API"],
-  }
+  },
 ]
+
