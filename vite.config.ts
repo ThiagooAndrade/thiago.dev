7538c7ebd54dba574ef6d2b1cfed902575dbe503
ThiagooAndrade <andradethiago1609@gmail.com>
2025-01-03 23:12:44 -0300

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: ['/foto-Thiago.jpeg'], // Adicione o caminho correto para a imagem
+    },
+  },
 })
